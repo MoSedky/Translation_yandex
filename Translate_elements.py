140bb9848c658f0a95f160b3a4a5cc2fc0426eed
MoSedky <35762138+MoSedky@users.noreply.github.com>
2018-11-28 17:46:12 +0300

    Update Translate_elements.py

--- Translate_elements.py
+++ Translate_elements.py
@@ -6,7 +6,7 @@ from yandex.Translater import Translater
 
 def translate():
     tr = Translater()
-    tr.set_key('trnsl.1.1.20181128T091344Z.1764de5129a6aa08.f7b484a88cbe7ec9a2f4d295a1c310ba5867d9cc') # Api key found on https://translate.yandex.com/developers/keys
+    tr.set_key('Insert_your_API_Key_Here') # Api key found on https://translate.yandex.com/developers/keys
     text_to_translate = 'شركة ذات مسؤولية محدودة'
     tr.set_text(text_to_translate)
     tr.set_from_lang('ar')
